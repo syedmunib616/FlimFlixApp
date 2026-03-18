5c5230ef533d31bd33cf021175a0dde914ded0d6
munib616 <syedmunibudden@gmail.com>
2026-03-18 04:14:01 -0500

    Initial Commit

---
+++ FilmFlix/FilmFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmFlixApp.swift
+//  FilmFlix
+//
+//  Created by muqeet syed on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct FilmFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
